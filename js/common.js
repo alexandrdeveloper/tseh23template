d1802a939eff9ac1a67bf9684f3a4c5a6f4a9179
Alexandr <33997516+alexwebdev96@users.noreply.github.com>
2021-05-08 16:43:55 +0300

    principles section

--- js/common.js
+++ js/common.js
@@ -10,14 +10,21 @@ $(function() {
 		$('.b-navbar-phone').toggleClass('b-navbar-phone_visible');
 	});
 
+	let accordeonHead = $('.theme-accordeon__section-head');
+	let accordeonHeadActiveClass = 'theme-accordeon__section-head_active';
+  
+	let accordeonClickToggle = function(e) {
+		e.preventDefault();
+	  accordeonHead.not(this).removeClass(accordeonHeadActiveClass).next().slideUp(200);
+	  $(this).toggleClass(accordeonHeadActiveClass).next().slideToggle(200);
+	};
+	//accordeonHead.filter(':first').toggleClass(accordeonHeadActiveClass).next().show();
+	accordeonHead.on('click', accordeonClickToggle);
 
-	let title = $('.section-title_backtext');
+
 
 	
 
-	title.on('scroll', function() {
-		let titleText = $(this).find('h2').text();
-		$(this).html(`<h2>${titleText}</h2><span>${titleText}</span>`)
-	});
+	
 
 });
